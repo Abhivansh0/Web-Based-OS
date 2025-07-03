068c310862293159c123ca8c8e959e340927a477
Mukund149 <mukundtaneja2004@gmail.com>
2025-07-03 16:19:37 +0530

    StorageSystemKernel

--- src/context/kernelContext.jsx
+++ src/context/kernelContext.jsx
@@ -1,8 +1,10 @@
 import ProcessManager from "../kernel/ProcessManager";
 import MemoryManager from "../kernel/MemoryManager";
 import Scheduler from "../kernel/Scheduler";
+import StorageSystem from "../kernel/StorageSystem"
 
 const osMemoryManager = new MemoryManager(2048)
+const osStorageSystem = new StorageSystem
 const osProcessManager = new ProcessManager(osMemoryManager)
 const osScheduler = new Scheduler(osProcessManager)
 osScheduler.startScheduler()
