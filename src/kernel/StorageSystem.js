b807c95b0e5bfde91c38fd34bd8561e84235e492
Mukund149 <mukundtaneja2004@gmail.com>
2025-07-03 16:11:53 +0530

    StorageSystem

---
+++ src/kernel/StorageSystem.js
@@ -0,0 +1,70 @@
+class StorageSystem {
+    constructor(totalStorageKB, blockSizeKB) {
+        this.totalKB = totalStorageKB
+        this.blockSizeKB = blockSizeKB
+        this.totalBlocks = Math.floor(this.totalKB / this.blockSizeKB)
+        this.disk = new Array(this.totalBlocks).fill(null)
+        this.fileTable = {}
+    }
+
+    allocateStorage(path, sizeKB) {
+        if (this.fileTable[path]) {
+            return false
+        }
+
+        const blocksNeeded = Math.ceil(sizeKB / this.blockSizeKB)
+        const freeBlocks = this.getFreeBlocksIndex()
+
+        if (freeBlocks.length < blocksNeeded) {
+            return false
+        }
+
+        const allocatedBlocks = freeBlocks.slice(0, blocksNeeded)
+        allocatedBlocks.forEach(index => this.disk[index] = path)
+
+        this.fileTable[path] = {
+            size: sizeKB,
+            blocks: allocatedBlocks,
+            content: ""
+        }
+
+        return true
+    }
+
+    deAllocateStorage(path) {
+        if (!this.fileTable[path]) {
+            return false
+        }
+
+        const deAllocatingBlocks = this.fileTable[path].blocks
+        deAllocatingBlocks.forEach(index => this.disk[index] = null)
+        delete this.fileTable[path]
+
+        return true
+    }
+
+    getFreeBlocksIndex() {
+        return this.disk.map((b, i) => b === null ? i : null).filter(i => i !== null)
+    }
+
+
+    getUsedStorage() {
+        return this.disk.filter(block => block !== null).length * this.blockSizeKB
+    }
+
+    getAvailableStorage() {
+        return this.totalKB - this.getUsedStorage()
+    }
+
+
+    listStorage() {
+        return Object.entries(this.fileTable).map(([path, meta]) => ({
+            path,
+            sizeKB: meta.size,
+            blocks: meta.blocks
+        }));
+    }
+
+}
+
+export default StorageSystem
